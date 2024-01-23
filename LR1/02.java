576c6687d1bdde02a5ac3653bdb5a910689d0a6d
Alisha <91132374+khairu11ina@users.noreply.github.com>
2024-01-23 11:55:05 +0300

    Create 02.java

---
+++ LR1/02.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+public class Program {
+
+
+public static void main(String[] args) {
+
+
+
+String line; line=JOptionPane.showInputDialog("Введите строку");
+JOptionPane.showMessageDialog(null, "Ваша строка: \n"+line);
+
+
+}
+
+
+}
+
